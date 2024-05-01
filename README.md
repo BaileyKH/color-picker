c3fe6c533fd3fada472be22ed628ffeb6d94de61
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-05-01 06:53:45 -0700

    color scheme generator

---
+++ README.md
@@ -0,0 +1,12 @@
+# Color Scheme Generator
+
+This application is a color scheme generator that allows users to select a color and a scheme mode to generate and display a variety of color schemes based on their choice. Users can see the color in both visual form and hex code, and can interact with the color outputs.
+
+## Features
+
+- **Color Selection:** Choose a base color using a color picker.
+- **Scheme Mode Selection:** Select a mode to generate color schemes, such as monochrome, analogic, complement, etc.
+- **Dynamic Color Scheme Output:** Displays color schemes based on the selected color and mode.
+- **Hex Code Display:** Shows hex codes for each color in the scheme, which are clickable for copying.
+
+
---
+++ index.js
@@ -0,0 +1,35 @@
+
+const colorPicker = document.getElementById("color-picker");
+const colorMode = document.getElementById("mode");
+const output = document.getElementById("color-container");
+const colorButton = document.getElementById("color-btn");
+const hexOutput = document.getElementById("hex-container");
+
+function handleColorGeneration() {
+    let color = colorPicker.value.slice(1);
+    let mode = colorMode.value;
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`)
+        .then(res => res.json())
+        .then(data => renderColors(data))
+}
+
+function renderColors(data) {
+    if (output) {
+        output.innerHTML = '';
+        hexOutput.innerHTML = ''
+        data.colors.forEach((color) => {
+            output.innerHTML += `<div class="colors" style="background-color:${color.hex.value};"></div>`;
+            hexOutput.innerHTML += `<div class="hex-codes"><p id="copy-hex" style="cursor: pointer;">${color.hex.value}</p></div>`
+            hexOutput.classList.remove("hidden")
+        });
+    }
+}
+
+colorButton.addEventListener("click", handleColorGeneration);
+
+
+
+
+
+
